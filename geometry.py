cfff5bee7341190e3e4a15074ac09ad240cd2642
Lennart Grosskreutz <lennart.grosskreutz@student.kit.edu>
2022-12-08 11:26:18 +0100

    Implement special case intersection line segment ray: directions parallel

--- geometry.py
+++ geometry.py
@@ -1,21 +1,48 @@
 import numpy as np
 
 class LineSegment:
+
     def __init__(self, a, b):
         #numpy arrays
         self.a = a
         self.b = b
 
 class Ray:
+
+    #if you want direction d from some angle theta:
+    #Use d = np.array([cos(theta), sin(theta)])
     def __init__(self, p, d):
         #numpy arrays
         self.p = p
         self.d = d
 
+#Return (boolIntersection, intersectionPoint)
+#if there are multiple intersection points, return the closest one in terms of the ray's parameter p
 def calculateIntersection(ray, lineSegment):
     d_prime = np.array([-ray.d[1], ray.d[0]])
-    #attention: division by zero possible
-    t_line = np.dot(d_prime, ray.p - lineSegment.a) / np.dot(d_prime, lineSegment.b - lineSegment.a)
+    denominator = np.dot(d_prime, lineSegment.b - lineSegment.a)
+    #treat special case first: denominator close to zero
+    epsilon = 0.005
+    if abs(denominator) < epsilon:        
+        if (np.linalg.norm(ray.p - lineSegment.a)) <= epsilon:
+            return (True, lineSegment.a)
+        #case 1.1: lineSegment parallel, but not aligned. No intersection
+        #no division by zero possible
+        comparison_direction = (ray.p - lineSegment.a) / (np.linalg.norm(ray.p - lineSegment.a))
+        #check if comparison direction and ray direction are equal are opposite to each other
+        if np.linalg.norm(ray.d - comparison_direction) < epsilon or np.linalg.norm(ray.d + comparison_direction) < epsilon:
+            dist_a = np.linalg.norm(ray.p - lineSegment.a)
+            dist_b = np.linalg.norm(ray.p - lineSegment.b)
+            if dist_a <= dist_b:
+                return (True, lineSegment.a)
+            else:
+                return (True, lineSegment.b)
+        else:
+            #not alligned, no intersection
+            return (False, np.array([0, 0]))
+
+    #regular case: lineSegment direction and ray direction not parallel
+    t_line = np.dot(d_prime, ray.p - lineSegment.a) / denominator
     t_ray = np.dot(- ray.p + lineSegment.a + t_line * (lineSegment.b - lineSegment.a), ray.d)
     if t_ray >= 0 and t_line >= 0 and t_line <= 1:
         intersectionPoint = lineSegment.a + t_line * (lineSegment.b - lineSegment.a)
--- testGeometry.py
+++ testGeometry.py
@@ -2,12 +2,47 @@ from geometry import *
 from math import sin, cos
 import math
 
-x_0 = 2
-a = np.array([x_0, 0])
-b = np.array([x_0, 20])
-p = np.array([0, 0])
-theta = math.pi / 6
-d = np.array([cos(theta), sin(theta)])
-lineSeg = LineSegment(a, b)
-ray = Ray(p, d)
-print(calculateIntersection(ray, lineSeg))
+def testRegular():
+    x_0 = 2
+    a = np.array([x_0, 0])
+    b = np.array([x_0, 20])
+    p = np.array([0, 0])
+    theta = math.pi / 6
+    d = np.array([cos(theta), sin(theta)])
+    lineSeg = LineSegment(a, b)
+    ray = Ray(p, d)
+    print(calculateIntersection(ray, lineSeg))
+
+def testParallelNoIntersection():
+    p = np.array([0, 0])
+    theta = math.pi / 6
+    d = np.array([cos(theta), sin(theta)])
+    a = np.array([2, 0])
+    b = a + d
+    lineSeg = LineSegment(a, b)
+    ray = Ray(p, d)
+    print(calculateIntersection(ray, lineSeg))
+
+def testParallelWithIntersectionA():
+    p = np.array([0, 0])
+    theta = math.pi / 6
+    d = np.array([cos(theta), sin(theta)])
+    a = 3 * d
+    b = a + d
+    lineSeg = LineSegment(a, b)
+    ray = Ray(p, d)
+    print(calculateIntersection(ray, lineSeg))
+
+def testParallelWithIntersectionB():
+    p = np.array([0, 0])
+    theta = math.pi / 6
+    d = np.array([cos(theta), sin(theta)])
+    a = 3 * d
+    b = a - d
+    lineSeg = LineSegment(a, b)
+    ray = Ray(p, d)
+    print(calculateIntersection(ray, lineSeg))
+
+if __name__ == "__main__":
+    #testParallelNoIntersection()
+    testParallelWithIntersectionB()
