7bb9493bf4c6b914a121c711158713508c724727
Lennart Grosskreutz <lennart.grosskreutz@student.kit.edu>
2023-01-03 13:56:53 +0100

    Environment add table legs

--- environment.py
+++ environment.py
@@ -13,10 +13,15 @@ class Environment:
     DOOR_X = 1000
     DOOR_Y = 150
     DOOR_WIDTH = 100
+    # table constants
+    TABLE_HEIGHT = 80
+    TABLE_WIDTH = 200
+    TABLE_LEG_LENGTH = 15
 
     def __init__(self):
         self.rectangles = []
         self.initSimpleMap()
+        self.drawFourTables(40, 40, 500)
 
     # calculates the distance to the closest obstacle from the position (x,y)
     # with gaze direction indicated by the angle theta
@@ -61,3 +66,21 @@ class Environment:
         self.rectangles.append(geo.RectAsQuadrangle(self.DOOR_X, self.DOOR_X + self.WALL_SIZE, 0, self.DOOR_Y))
         # door, lower part
         self.rectangles.append(geo.RectAsQuadrangle(self.DOOR_X, self.DOOR_X + self.WALL_SIZE, self.DOOR_Y + self.DOOR_WIDTH, self.HEIGHT))
+
+    def drawFourTables(self, gapX, gapY, gapSecondX):
+        self.drawTableLegs(gapX, gapY, self.TABLE_LEG_LENGTH, self.TABLE_HEIGHT, self.TABLE_WIDTH)
+        self.drawTableLegs(gapX, self.HEIGHT - gapY - self.TABLE_HEIGHT, self.TABLE_LEG_LENGTH, self.TABLE_HEIGHT, self.TABLE_WIDTH)
+        self.drawTableLegs(gapSecondX, gapY, self.TABLE_LEG_LENGTH, self.TABLE_HEIGHT, self.TABLE_WIDTH)
+        self.drawTableLegs(gapSecondX, self.HEIGHT - gapY - self.TABLE_HEIGHT, self.TABLE_LEG_LENGTH, self.TABLE_HEIGHT, self.TABLE_WIDTH)
+        
+
+    # draw four legs of table that is aligned with coordinate system
+    def drawTableLegs(self, x, y, tableLegLength, tableHeight, tableWidth):
+        # top left
+        self.rectangles.append(geo.RectAsQuadrangle(x, x + tableLegLength, y, y + tableLegLength))
+        # top right
+        self.rectangles.append(geo.RectAsQuadrangle(x + tableWidth - tableLegLength, x + tableWidth, y, y + tableLegLength))
+        # bottom left
+        self.rectangles.append(geo.RectAsQuadrangle(x, x + tableLegLength, y + tableHeight - tableLegLength, y + tableHeight))
+        # bottom right
+        self.rectangles.append(geo.RectAsQuadrangle(x + tableWidth - tableLegLength, x + tableWidth, y + tableHeight - tableLegLength, y + tableHeight))
