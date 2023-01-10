d3471ce0080e5f835df8407a86eac3fe0cc29e38
Lennart Grosskreutz <lennart.grosskreutz@student.kit.edu>
2023-01-10 20:21:48 +0100

    Remove unnecessary methods in algo and array utils

--- algorithm.py
+++ algorithm.py
@@ -34,85 +34,24 @@ class Algorithm(threading.Thread):
     def run(self):
         self.scannerAlgorithm()
 
+    # our algorithm
     def scannerAlgorithm(self):
-        D_DOOR = 300
-
         dt = 0
         lasttime = datetime.now()
         while(self.running): # basically while(true)
-            # time difference to last action
+            # time difference to last action: for the simulation
             dt = (datetime.now() - lasttime).total_seconds()
             lasttime = datetime.now()
             measurements = self.robotRotationMeasuring()
-            self.printArray(measurements)
             centerLineFound = self.searchCenterLineAndMove(measurements)
             if not centerLineFound:
                 self.robotMoveRandomly(measurements)
-                
-                        
-    def detectDoor(self, measurements):   
-        CUTOFF_DELTA = 500
-        MIN_DOOR_INDEX_WIDTH = round(ROTATION_STEPS / 36)
-        print("MEASUREMENTS")
-        self.printArray(measurements)
-        # TODO list of drop and increase indices
-        dropIndexList = []
-        increaseIndexList = []
-        for i in range(ROTATION_STEPS):
-            if measurements[i - 1] + CUTOFF_DELTA < measurements[i]:
-                dropIndexList.append(i)
-            if measurements[i] > measurements[(i + 1) % len(measurements)] + CUTOFF_DELTA:
-                increaseIndexList.append(i)
-        # find index of angle corresponding to door
-        (doorDetected, doorStartIndex, doorEndIndex) = arrayUtils.detectDoor(dropIndexList, increaseIndexList, MIN_DOOR_INDEX_WIDTH, ROTATION_STEPS)
-        if not doorDetected:
-            return (False, 0)
-        # door detected, calculate door middle
-        indexDoorMiddle = -1
-        # calculate estimated middle of the door
-        if doorStartIndex < doorEndIndex:
-            indexDoorMiddle = round(0.5 * (doorEndIndex + doorStartIndex))
-        else:
-            indexDoorMiddle = round((0.5 * (doorStartIndex + doorEndIndex + len(measurements))) % len(measurements))
-        print("drop index   door index   increaseIndex", (doorStartIndex, indexDoorMiddle, doorEndIndex))
-        return (doorDetected, indexDoorMiddle)
-
-    def detectDoorSpecific(self, wallDistance):
-        # TODO values shall depend on wall distance and door position
-        START_SCAN_INDEX = 320
-        END_SCAN_INDEX = 10
-        ROTATION_AMOUNT = (ROTATION_STEPS + END_SCAN_INDEX - START_SCAN_INDEX) % ROTATION_STEPS
-        CUTOFF_DELTA = 100
-        D_DOOR = 250
-        # good rotation is ensured 
-        # set up for scan
-        self.robotPartialRotation(START_SCAN_INDEX)
-        measurements = self.robotRotationMeasuring(myRotationSteps = ROTATION_AMOUNT)
-        dropIndexList = []
-        increaseIndexList = []
-        for i in range(len(measurements)):
-            if measurements[i - 1] + CUTOFF_DELTA < measurements[i]:
-                dropIndexList.append(i)
-            if measurements[i] > measurements[(i + 1) % len(measurements)] + CUTOFF_DELTA:
-                increaseIndexList.append(i)
-        # because we only did a partial rotation, we expect only one drop and one increase index
-        if (len(dropIndexList) == 1) and (len(increaseIndexList) == 1):
-            indexDoorMiddle = round(0.5 * (dropIndexList[0] + increaseIndexList[0]))
-            # make the robot look to the door middle
-            self.robotPartialRotation(ROTATION_STEPS - ROTATION_AMOUNT + indexDoorMiddle)
-            # make a big step through the door
-            self.robotMoveForwardAnimated(D_DOOR)
-            return True
-        # door not detected, make sure robot is looking in same direction as previously
-        self.robotPartialRotation(ROTATION_STEPS - ROTATION_AMOUNT + ROTATION_STEPS - START_SCAN_INDEX)
-        return False
 
     # searches the center line by analyzing the measurements.
     # if robot already on sensor line: follow it
     # else try to navigate robot in the direction of center line
     # return false if no clue where center line is, in that case robot was not moved
     # else return true
-    # TODO on center line but wall ahead
     def searchCenterLineAndMove(self, measurements):
         OPPOSITE_TOLERANCE = 2
         WALL_DISTANCE_EPSILON = 10
@@ -183,9 +122,41 @@ class Algorithm(threading.Thread):
                     self.robotMoveForwardAnimated(min(D_ML_APPROACHING, abs(distanceUpperWall - BOARD_HEIGHT / 2)))
         return spotsFound
 
+    # try to detect the door in a certain angle range, not in a full scan
+    # under the assumption the robot is already on the middle line and orientated correctly
+    def detectDoorSpecific(self, wallDistance):
+        # TODO values shall depend on wall distance and door position
+        START_SCAN_INDEX = 320
+        END_SCAN_INDEX = 10
+        ROTATION_AMOUNT = (ROTATION_STEPS + END_SCAN_INDEX - START_SCAN_INDEX) % ROTATION_STEPS
+        CUTOFF_DELTA = 100
+        D_DOOR = 250
+        # good rotation is ensured 
+        # set up for scan
+        self.robotPartialRotation(START_SCAN_INDEX)
+        measurements = self.robotRotationMeasuring(myRotationSteps = ROTATION_AMOUNT)
+        dropIndexList = []
+        increaseIndexList = []
+        for i in range(len(measurements)):
+            if measurements[i - 1] + CUTOFF_DELTA < measurements[i]:
+                dropIndexList.append(i)
+            if measurements[i] > measurements[(i + 1) % len(measurements)] + CUTOFF_DELTA:
+                increaseIndexList.append(i)
+        # because we only did a partial rotation, we expect only one drop and one increase index
+        if (len(dropIndexList) == 1) and (len(increaseIndexList) == 1):
+            indexDoorMiddle = round(0.5 * (dropIndexList[0] + increaseIndexList[0]))
+            # make the robot look to the door middle
+            self.robotPartialRotation(ROTATION_STEPS - ROTATION_AMOUNT + indexDoorMiddle)
+            # make a big step through the door
+            self.robotMoveForwardAnimated(D_DOOR)
+            return True
+        # door not detected, make sure robot is looking in same direction as previously
+        self.robotPartialRotation(ROTATION_STEPS - ROTATION_AMOUNT + ROTATION_STEPS - START_SCAN_INDEX)
+        return False
+
     # TODO maybe try something more random
     def robotMoveRandomly(self, measurements):
-        SAFETY_DISTANCE = 100
+        SAFETY_DISTANCE = 250
         MOVING_DISTANCE = 70
         AMOUNT_TURN = round(ROTATION_STEPS / 8)
         print("Falling back to random behaviour")
@@ -193,10 +164,25 @@ class Algorithm(threading.Thread):
             self.robotPartialRotation(AMOUNT_TURN)
         self.robotMoveForwardAnimated(MOVING_DISTANCE)
 
+    # do full and partial rotation and collect sensor measurements
+    def robotRotationMeasuring(self, myRotationSteps = ROTATION_STEPS):
+        measurements = []
+        for i in range(myRotationSteps):
+            measurements.append(self.getRobotDistToObstacle())
+            self.robotTurn(2 * math.pi / ROTATION_STEPS)
+            time.sleep(ROTATION_TIME / ROTATION_STEPS)
+        return measurements
+
+    # return the distance of the robot to the next obstacle in the robot's gaze direction
+    def getRobotDistToObstacle(self, cutoff = True):
+        CUTOFF_VALUE = 300
+        realDistance = self.environment.getDistanceToObstacle(self.robot.x, self.robot.y, self.robot.theta)
+        if cutoff:
+            return min(realDistance, CUTOFF_VALUE)
+        return realDistance
+
     # just for the simulation
     def robotMoveForwardAnimated(self, distance):
-        # TODO use
-        
         FPS = 30
         forwardStep = MOVING_SPEED / FPS
         distanceLeft = distance
@@ -208,34 +194,30 @@ class Algorithm(threading.Thread):
         if distanceLeft > 0:
             self.robotMoveForward(distanceLeft)
 
-    # very simple algorithm for testing
-    def backAndForthAlgorithm(self):
-        vel = 5
-        dt = 0
-        lasttime = datetime.now()
-        sign = 1
-        while(self.running):
-            # time difference to last action
-            dt = (datetime.now() - lasttime).total_seconds()
-            lasttime = datetime.now()
+    # move forward by bypassing actual control parameters (assuming we have inverse kinematics solved)
+    def robotMoveForward(self, distance):
+        self.robot.x += cos(self.robot.theta) * distance
+        self.robot.y += sin(self.robot.theta) * distance
 
-            # modify state of robot respectively to passed time and the environment
-            # if close to obstacle, go to the other direction
-            if self.getRobotDistToObstacle() < 10:
-                sign = -sign
-                self.robot.theta += math.pi
-            # simulate calculation that takes some time
-            time.sleep(0.5)
-            self.robot.x += sign * dt * vel
+    # make the robot turn clockwise by the given amount in rad
+    def robotTurn(self,amount):
+        self.robot.theta += amount
 
-    # return the distance of the robot to the next obstacle in the robot's gaze direction
-    def getRobotDistToObstacle(self, cutoff = True):
-        CUTOFF_VALUE = 300
-        realDistance = self.environment.getDistanceToObstacle(self.robot.x, self.robot.y, self.robot.theta)
-        if cutoff:
-            return min(realDistance, CUTOFF_VALUE)
-        return realDistance
+    # make a partial rotation without taking measurements
+    def robotPartialRotation(self, steps):
+        # avoid more than one rotation
+        steps = steps % ROTATION_STEPS
+        if steps < ROTATION_STEPS / 2:
+            for i in range(steps):
+                self.robotTurn(2 * math.pi / ROTATION_STEPS)
+                time.sleep(ROTATION_TIME / ROTATION_STEPS)
+        else:
+            # more efficient to rotate in the other direction
+            for i in range(ROTATION_STEPS - steps):
+                self.robotTurn(- 2 * math.pi / ROTATION_STEPS)
+                time.sleep(ROTATION_TIME / ROTATION_STEPS)
 
+    # JUST FOR TESTING
     # control wheel velocities with keyboard, w-s-e-d keys
     def manualControlAlgorithm(self):
         STEP = 5
@@ -246,7 +228,6 @@ class Algorithm(threading.Thread):
         while(self.running):
             # time difference to last action
             dt = (datetime.now() - lasttime).total_seconds()
-
             # fetch keypress
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -266,113 +247,3 @@ class Algorithm(threading.Thread):
             if dt > (1 / UPDATES_PER_SECOND):
                 lasttime = datetime.now()
                 self.robot.move(v_l, v_r, dt)
-
-    # manual control that sets x, y, theta of robot directly (not through control variables, namely wheel velocities)
-    # use s to turn left, d to turn right
-    def manualControlAlgorithmCheating(self):
-        dt = 0
-        lasttime = datetime.now()
-        while(self.running):
-            # time difference to last action
-            dt = (datetime.now() - lasttime).total_seconds()
-            
-            # fetch keypress
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                if event.type == pygame.KEYDOWN and event.key==pygame.K_s:
-                    self.robotTurnLeft()
-                if event.type == pygame.KEYDOWN and event.key==pygame.K_d:
-                    self.robotTurnRight()
-            # only move every split second
-            if dt > (1 / UPDATES_PER_SECOND):
-                lasttime = datetime.now()
-                self.robotMoveForward(1)
-
-    # search for the middle of the door by determining the two spots where the measured distance suddenly increases / drops significantly
-    # go some distance in the direction of that spot
-    def scannerAlgorithmSimple(self):
-        epsilon = 10
-        cutoffDelta = 40
-        forwardAfterRotation = 300 # shall be a whole number
-        dt = 0
-        lasttime = datetime.now()
-        while(self.running):
-            # time difference to last action
-            dt = (datetime.now() - lasttime).total_seconds()
-            lasttime = datetime.now()
-            measurements = self.robotRotationMeasuring()
-            # search measurements for the two closest points on each horizontal wall
-            # distance roughly BOARD_HEIGHT to each other
-            # search for four non-monotone spots
-            monotoneIndices = []
-            monotoneValues = []
-            dropIndex = -1
-            increaseIndex = -1
-            for i in range(ROTATION_STEPS):
-                if measurements[i - 1] < measurements[i] and measurements[i] < measurements[(i + 1) % len(measurements)]:
-                    monotoneIndices.append(i)
-                if measurements[i - 1] + cutoffDelta < measurements[i]:
-                    dropIndex = i
-                elif measurements[i] > measurements[(i + 1) % len(measurements)] + cutoffDelta:
-                    increaseIndex = i
-            # find index of angle corresponding to door
-            if dropIndex < increaseIndex:
-                doorIndex = round(0.5 * (increaseIndex + dropIndex))
-            else:
-                doorIndex = round((0.5 * (dropIndex + increaseIndex + len(measurements))) % len(measurements))
-            print("drop index   door index   increaseIndex", (dropIndex, doorIndex, increaseIndex))
-            # rotate to the spot where we assume the middle of the door to be
-            time.sleep(1)
-            # self.robotPartialRotation(doorIndex)
-            self.robotTurn(2 * math.pi * (doorIndex / ROTATION_STEPS))
-
-            # go forward some distance
-            distanceToGo = forwardAfterRotation
-            while distanceToGo > 0:
-                self.robotMoveForward(1)
-                time.sleep(0.01)
-                distanceToGo -= 1
-            
-
-    # assuming we have solved inverse kinematics, we can tell the robot to drive forward and to turn some angle directly
-
-    def robotMoveForward(self, distance):
-        self.robot.x += cos(self.robot.theta) * distance
-        self.robot.y += sin(self.robot.theta) * distance
-
-    # make the robot turn clockwise by the given amount in rad
-    def robotTurn(self,amount):
-        self.robot.theta += amount
-
-    def robotTurnRight(self):
-        self.robotTurn(math.pi / 2)
-
-    def robotTurnLeft(self):
-        self.robotTurn(- (math.pi / 2))
-
-    def robotRotationMeasuring(self, myRotationSteps = ROTATION_STEPS):
-        measurements = []
-        for i in range(myRotationSteps):
-            measurements.append(self.getRobotDistToObstacle())
-            self.robotTurn(2 * math.pi / ROTATION_STEPS)
-            time.sleep(ROTATION_TIME / ROTATION_STEPS)
-        return measurements
-
-    # make a partial rotation without taking measurements
-    def robotPartialRotation(self, steps):
-        # avoid more than one rotation
-        steps = steps % ROTATION_STEPS
-        if steps < ROTATION_STEPS / 2:
-            for i in range(steps):
-                self.robotTurn(2 * math.pi / ROTATION_STEPS)
-                time.sleep(ROTATION_TIME / ROTATION_STEPS)
-        else:
-            # more efficient to rotate in the other direction
-            for i in range(ROTATION_STEPS - steps):
-                self.robotTurn(- 2 * math.pi / ROTATION_STEPS)
-                time.sleep(ROTATION_TIME / ROTATION_STEPS)
-
-    def printArray(self, array):
-        for i in range(0, len(array)):
-            print(str(i) + "   " + str(array[i]))
